aae1909bfe5ffbfe8e99d42c2e64f3db741d363c
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-26 19:36:07 +0000

    Switch to the Euler-Richardson algorithm

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,6 +1,6 @@
 public class Simulation {
   Simulation(double theta0, double omega0) {
-    double theta, omega, alpha, t, dt, tmax;
+    double theta, thetaMid, omega, omegaMid, alpha, alphaMid, t, dt, tmax;
     t = 0;
     dt = 0.01;
     tmax = 100;
@@ -10,8 +10,11 @@ public class Simulation {
     omega = omega0;
     while (t < tmax) {
       alpha = -Math.sin(theta);
-      theta += omega * dt;
-      omega += alpha * dt;
+      thetaMid = theta + omega * 0.5 * dt;
+      omegaMid = omega + alpha * 0.5 * dt;
+      alphaMid = -Math.sin(thetaMid);
+      theta += omegaMid * dt;
+      omega += alphaMid * dt;
       t += dt;
       plot.addPoint(t, theta);
     }
