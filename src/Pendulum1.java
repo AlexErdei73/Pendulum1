fe9f1c8885267a618afe2846ed58d28bb0306d94
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-27 12:43:48 +0000

    Add angleInDegree command line parameter

--- src/Pendulum1.java
+++ src/Pendulum1.java
@@ -1,5 +1,12 @@
 public class Pendulum1 {
     public static void main(String[] args) {
-      new Simulation(0.01, 0);
+        double angleInDegree = 1;
+        try {
+            angleInDegree = Double.parseDouble(args[0]);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            System.out.println("Missing command line parameter");
+            System.out.println("Default angle = " + angleInDegree + " degrees");
+        }
+        new Simulation(angleInDegree / 180 * Math.PI, 0);
     }
 }
